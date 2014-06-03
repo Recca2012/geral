4e554f59f677c03599ebd800f74aa098b2be1585
Mauricio Fernandes <mauriciofernandes1991@gmail.com>
2014-06-03 15:27:36 -0300

    teste2

--- ui.R
+++ ui.R
@@ -20,7 +20,7 @@ shinyUI(fluidPage(navbarPage("Canceres", id="nav",
                    ),
                    tabPanel('Descritivas e graficos gerais',
                             img(src="incidencia-de-canceres-analise.jpg", height = "1800px", width = "900px"),
-                            <p>"Os canceres são mostrados em ordem decrescente de frequencia. O cancer mais comum foi o Cancer no sistema genital masculino com uma frequencia de 2 milhoes nos anos em analise."</p>,
+                            p("Os canceres são mostrados em ordem decrescente de frequencia. O cancer mais comum foi o Cancer no sistema genital masculino com uma frequencia de 2 milhoes nos anos em analise."),
                             img(src="porcentagem-acumulada-da-incidencia-analise.jpg", height = "1800px", width = "900px"),
                             img(src="mapa-de-correlacao-analise.jpg", height = "900px", width = "900px")
                             
